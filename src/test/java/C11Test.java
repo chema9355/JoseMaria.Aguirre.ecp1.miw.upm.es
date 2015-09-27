787008fa538b54608ef26456df6221c272e9f289
chema9355 <chema9355@hotmail.com>
2015-09-27 17:28:42 +0200

    Creacion C11Test

---
+++ src/test/java/C11Test.java
@@ -0,0 +1,26 @@
+	
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C11Test {
+
+	public class PointTest {
+	    private C11 c11;
+
+	    @Before
+	    public void before() {
+	        c11 = new C11();
+	    }
+
+	    @Test
+	    public void testm1() {
+	        assertEquals("m1", c11.m1());
+	    }
+	    
+	    @Test
+	    public void testm2() {
+	        assertEquals("m2", c11.m2());
+	    }
+}}
